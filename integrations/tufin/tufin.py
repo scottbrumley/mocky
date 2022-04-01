c37d4fc2f80b0f28643d584a4f12b803e77ffe77
Scott Brumley <scott@nsisecure.com>
2022-04-01 17:04:50 +0000

    Added Tufin Devices and Basic Authentication

--- integrations/tufin/tufin.py
+++ integrations/tufin/tufin.py
@@ -1,25 +1,49 @@
 from flask import Blueprint
+from flask_httpauth import HTTPBasicAuth
 from flask import request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
+auth = HTTPBasicAuth()
 tufin_bp = Blueprint('tufin_bp', __name__)
 
+# Hard Code User and Password
+user = 'tufin'
+pw = 'tufin'
 
-@tufin_bp.route('/securechangeworkflow/api/secureapp/repository/applications/{}/connections/<app_id>', methods=['GET'])
-def api_all(ip):
-    ip_info = {
-        "anycast": True,
-        "city": "Los Angeles",
-        "country": "US",
-        "hostname": "one.one.one.one",
-        "ip": ip,
-        "lat": 34.0522,
-        "lng": -118.2437,
-        "loc": "34.0522,-118.2437",
-        "org": "AS13335 Cloudflare, Inc.",
-        "postal": "90084",
-        "readme": "https://tufin.io/missingauth",
-        "region": "California",
-        "timezone": "America/Los_Angeles"
+users = {
+    user: generate_password_hash(pw)
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
+
+
+@tufin_bp.route('/securechangeworkflow/api/securechange/devices', methods=['GET'])
+@auth.login_required
+def api_all():
+    devices_info = {
+        "devices":
+               {"device": [   
+                               {
+                               "id": 553,
+                               "name": request.args.get('name'),
+                               "ip": "9.9.9.9",
+                               "vendor": "Cisco",
+                               "device_type": "asa"
+                               },
+                               {
+                               "id": 2,
+                               "name": "cisco_device_name",
+                               "ip": "7.7.7.7",
+                               "vendor": "Cisco",
+                               "device_type": "router"
+                               }
+                          ]
+                }
+
     }
-    return jsonify(ip_info)
+    return jsonify(devices_info)
--- mocky.py
+++ mocky.py
@@ -3,6 +3,7 @@ from flask import Flask
 ### Import Blue Prints ###
 #Import IP Info Blue Print
 from integrations.ipinfo.ipinfo import ipinfo_bp 
+from integrations.tufin.tufin import tufin_bp 
 
 app = Flask(__name__)
 
@@ -12,5 +13,6 @@ app.config["DEBUG"] = True
 
 #Register Vedor Simulation IP Info
 app.register_blueprint(ipinfo_bp)
+app.register_blueprint(tufin_bp)
 
 app.run(host="0.0.0.0")
