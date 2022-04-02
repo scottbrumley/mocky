3dfaa76bd6c27d038596b42826fbbe84d5ebf889
Scott Brumley <scott@nsisecure.com>
2022-04-02 18:47:28 +0000

    Adding Testing

---
+++ integrations/tufin/test_tufin.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestMocky(unittest.TestCase):
+
+    def test_get_ents_returns_dictionary_given_empty_string(self):
+        ner = NamedEntityClient()
+        ents = ner.get_ents("")
+        self.assertIsInstance(ents, dict)
+
+
