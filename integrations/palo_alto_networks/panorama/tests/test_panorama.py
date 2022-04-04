8edae79aecaa030e9309dfcd61f44021a9a9537f
scottbrumley <scott@nsisecure.com>
2022-04-04 16:37:00 -0400

    Fixed simple xml error

--- integrations/palo_alto_networks/panorama/tests/test_panorama.py
+++ integrations/palo_alto_networks/panorama/tests/test_panorama.py
@@ -2,13 +2,13 @@ import unittest
 import requests
 from integrations.palo_alto_networks.panorama.panorama import panorama_url, user, pw
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 SERVER = "https://localhost:5000"
 
 
 class TestPanorama(unittest.TestCase):
     def test_http_response_status_200(self):
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         test_url = SERVER + panorama_url + "/api"
         response = requests.get(test_url, auth=(user,pw), verify=False)
         assert response.status_code == 200
